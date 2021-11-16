9dede74053c9d7fa6b15e09e611362d7875ca4d4
Will Liu <leoLW923@outlook.com>
2021-11-16 10:41:44 +0800

    add:新增base返回值枚举类

---
+++ src/main/java/com/lw/constant/BaseResult.java
@@ -0,0 +1,36 @@
+package com.lw.constant;
+
+/**
+ * @className: BaseResult
+ * @author: wei.liu2mars@outlook.com  lw
+ * @date: 2021/11/16 10:35
+ **/
+public enum BaseResult {
+    SUCCESS("0","success"),
+    FAIL("-1","error")
+    ;
+
+    private String code;
+    private String msg;
+
+    BaseResult(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
